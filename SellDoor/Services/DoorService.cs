fae7cffab889191a670515ba10e22a3cb37cd70d
MCrow <michalwrona@hotmail.com>
2024-11-28 10:42:24 +0100

    update version tag and rename doors.json to door.{doorName}.json

--- SellDoor/Services/DoorService.cs
+++ SellDoor/Services/DoorService.cs
@@ -1,5 +1,7 @@
-﻿using RestoreMonarchy.SellDoor.Database;
+﻿using Microsoft.SqlServer.Server;
+using RestoreMonarchy.SellDoor.Database;
 using SDG.Unturned;
+using System.IO;
 using UnityEngine;
 
 namespace RestoreMonarchy.SellDoor.Services
@@ -12,7 +14,18 @@ namespace RestoreMonarchy.SellDoor.Services
 
         void Awake()
         {
-            database = new DoorsDatabase(pluginInstance.Directory, "doors.json");
+            string fileName = $"Doors.{Provider.map.Replace(" ", "_")}.json";
+            string path = Path.Combine(pluginInstance.Directory, "doors.json");
+            if (File.Exists(path))
+            {
+                string path2 = Path.Combine(pluginInstance.Directory, fileName);
+                if (!File.Exists(path2))
+                {
+                    File.Move(path, path2);
+                }
+            }
+
+            database = new DoorsDatabase(pluginInstance.Directory, fileName);
         }
 
         void Start()
